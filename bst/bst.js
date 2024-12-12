61111faa2cece68041c3194c9243f625476b7306
daknine10 <156335553+daknine10@users.noreply.github.com>
2024-12-12 15:31:07 +0000

    Added functions for order callbacks. levelOrder, inOrder, preOrder, postOrder all take a parameter function and traverses the tree in the given order. height function added: gives the height of the node depth function added: returns the depth of a given node. isBalanced function added: checks if tree is balanced.

--- bst/bst.js
+++ bst/bst.js
@@ -31,8 +31,8 @@ class Tree {
 
         if (root.data === value) throw Error("Value already exists in the tree")
 
-        if (value < root.data) root.left = insert(value, root.left)
-        else if (value > root.data) root.right = insert(value, root.right)
+        if (value < root.data) root.left = this.insert(value, root.left)
+        else if (value > root.data) root.right = this.insert(value, root.right)
 
         return root;
     }
@@ -43,25 +43,123 @@ class Tree {
     }
 
     if (root.data > value) {
-        root.left = deleteItem(value, root.left);
-    } else if (root.data < value) {
-        root.right = deleteItem(value, root.right;
-    } else {
-
-        if (root.left === null) 
-            return root.right;
-
-        if (root.right === null) 
-            return root.left;
+        root.left = this.deleteItem(value, root.left);
+    } 
+    else if (root.data < value) {
+        root.right = this.deleteItem(value, root.right);
+    } 
+    else {
+        if (root.left === null) return root.right;
+        if (root.right === null) return root.left;
 
         let closest = root.right;
         while (closest.left !== null) {
             closest = closest.left
         }
-        root.value = closest.value;
-        root.right = deleteItem(root.right, closest.value)
+        root.data = closest.data;
+        root.right = this.deleteItem(closest.data, root.right)
     }
     return root;
+    }
+
+    find(value, root = this.root) {
+        if (root === null) return root
+        if (root.data === value) return root
+
+        if (value < root.data) return this.find(value, root.left)
+        else if (value > root.data) return this.find(value, root.right)
+
+        return root;
+    }
+
+    levelOrder(callback, root = this.root) {
+        if (!callback || typeof callback !== "function") {
+            throw new Error("This function accepts only callback functions as a parameter!");
+        }
+    
+        const q = [];
+        if (root !== null) {
+            q.push(root);
+        }
+    
+        while (q.length > 0) {
+            const node = q.shift();
+            callback(node);
+    
+            if (node.left !== null) {
+                q.push(node.left);
+            }
+            if (node.right !== null) {
+                q.push(node.right);
+            }
+        }
+    }
+
+    inOrder(callback, root = this.root) {
+        if (!callback || typeof callback !== "function") {
+            throw new Error("This function accepts only callback functions as a parameter!");
+        }
+
+        if (root === null) return;
+
+        this.inOrder(callback, root.left);
+        callback(root);
+        this.inOrder(callback, root.right); 
+    }
+
+    preOrder(callback, root = this.root) {
+        if (!callback || typeof callback !== "function") {
+            throw new Error("This function accepts only callback functions as a parameter!");
+        }
+
+        if (root === null) return;
+
+        callback(root);
+        this.preOrder(callback, root.left);
+        this.preOrder(callback, root.right); 
+    }
+
+    postOrder(callback, root = this.root) {
+        if (!callback || typeof callback !== "function") {
+            throw new Error("This function accepts only callback functions as a parameter!");
+        }
+
+        if (root === null) return;
+
+        this.preOrder(callback, root.left);
+        this.preOrder(callback, root.right); 
+        callback(root);
+    }
+
+    height(node, root = this.root) {
+        if (root === null) return 0;
+        
+        const leftHeight = this.height(node, root.left);
+        const rightHeight = this.height(node, root.right);
+    
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    depth(node, root = this.root, depth = 0) {
+        if (root === null) return -1;
+        if (root.data === node) return depth;
+
+        if (root.data > node) {
+            return this.depth(node, root.left, depth + 1);
+        } 
+        else if (root.data < node) {
+            return this.depth(node, root.right, depth + 1);
+        } 
+    }
+
+    isBalanced(root = this.root) {
+        if (root === null) return true;
+        
+        const leftHeight = this.height(root.left);
+        const rightHeight = this.height(root.right);
+
+        return Math.abs(leftHeight - rightHeight) <= 1  && this.isBalanced(root.left) && this.isBalanced(root.right);
+    }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -79,6 +177,12 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 const testTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 testTree.insert(25)
+testTree.deleteItem(67)
+testTree.deleteItem(324)
+testTree.deleteItem(5)
+testTree.deleteItem(7)
+testTree.find(23)
+
 
 
 prettyPrint(testTree.root);
